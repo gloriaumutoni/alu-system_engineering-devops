28f7edb2435633aac02bcc6b205367106da2df8d
Gloria Umutoni <124312203+gloriaumutoni@users.noreply.github.com>
2024-08-06 22:40:40 +0200

    Delete web_stack_debugging_3

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# Fix apache and automate it using Puppet
-exec { 'fixing typo...':
-  onlyif  => 'test -e /var/www/html/wp-settings.php',
-  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
